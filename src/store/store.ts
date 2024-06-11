198271a68010575bed6aed9813864540aedbd4ab
jiyoun0712 <jiyoun0712@gmail.com>
2024-06-11 17:00:01 +0900

    first

---
+++ src/store/store.ts
@@ -0,0 +1,45 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import CustomizerReducer from "./customizer/CustomizerSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+export const store = configureStore({
+    reducer: {
+    //   counter: counterReducer,
+       customizer: persistReducer<any>(persistConfig, CustomizerReducer),
+    //   ecommerceReducer: EcommerceReducer,
+    //   chatReducer: ChatsReducer,
+    //   emailReducer: EmailReducer,
+    //   notesReducer: NotesReducer,
+    //   contactsReducer: ContactsReducer,
+    //   ticketReducer: TicketReducer,
+    //   userpostsReducer: UserProfileReducer,
+    //   blogReducer: BlogReducer,
+    },
+    devTools: process.env.NODE_ENV !== "production",
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false, immutableCheck: false }),
+  });
+  const rootReducer = combineReducers({
+    // counter: counterReducer,
+     customizer: CustomizerReducer,
+    // ecommerceReducer: EcommerceReducer,
+    // chatReducer: ChatsReducer,
+    // emailReducer: EmailReducer,
+    // notesReducer: NotesReducer,
+    // contactsReducer: ContactsReducer,
+    // ticketReducer: TicketReducer,
+    // userpostsReducer: UserProfileReducer,
+    // blogReducer: BlogReducer,
+  });
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppState = ReturnType<typeof rootReducer>;
